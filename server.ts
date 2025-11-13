eebd22c461191e4c2be0bebe6b40779c894af2ad
Miguel Angelo <m.angelodev@gmail.com>
2025-11-13 16:42:55 -0300

    Cria primeira instancia do servidor

---
+++ server.ts
@@ -0,0 +1,20 @@
+#! Para executar é necessário utilizar as variaveis de ambientes informadas no README.md
+import express from 'express'
+import cors from 'cors'
+
+const PORT = process.env.PORT || 3000;
+
+const server = express()
+
+server.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  }
+))
+
+server.use(express.json());
+
+
+server.listen(PORT, () => {
+  console.log(`Triibo-Films API is running on port: ${PORT}`)
+})
