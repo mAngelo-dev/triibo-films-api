347d203f085dc2ffa38afdd66e23b13ffcd1db39
Miguel Angelo <m.angelodev@gmail.com>
2025-11-19 11:56:09 -0300

    Adiciona documentação da rota delete e put

--- routes/filmsRoute.ts
+++ routes/filmsRoute.ts
@@ -67,6 +67,16 @@ filmsRoute.get("/", filmsController.getAllFilms);
  *               details: {
  *
  *               }
+ *       401:
+ *          description: Não autorizado
+ *          content:
+ *            application/json:
+ *              schema:
+ *                type: object
+ *                properties:
+ *                  message:
+ *                    type: string
+ *                    example: "Não foi possível válidar suas credenciais. Por favor, realize novamente o login"
  *       404:
  *         description: Filme não encontrado
  */
@@ -101,11 +111,114 @@ filmsRoute.get("/:id", filmsController.getFilmById);
  *                   type: string
  *                 filmId:
  *                   type: string
+ *       401:
+ *          description: Não autorizado
+ *          content:
+ *            application/json:
+ *              schema:
+ *                type: object
+ *                properties:
+ *                  message:
+ *                    type: string
+ *                    example: "Não foi possível válidar suas credenciais. Por favor, realize novamente o login"
  *       500:
  *         description: Algo deu errado ao criar o filme.
  */
 filmsRoute.post("/", filmsController.createFilm);
+/**
+ * @openapi
+ * /films/{id}:
+ *   delete:
+ *     tags:
+ *       - Filmes
+ *     summary: Deleta um filme pelo ID
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: ID do filme a ser deletado
+ *         schema:
+ *           type: string
+ *           example: "10sdks1203912"
+ *     responses:
+ *       200:
+ *         description: Filme deletado com sucesso
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: "Filme deletado com sucesso."
+ *       401:
+ *          description: Não autorizado
+ *          content:
+ *            application/json:
+ *              schema:
+ *                type: object
+ *                properties:
+ *                  message:
+ *                    type: string
+ *                    example: "Não foi possível válidar suas credenciais. Por favor, realize novamente o login"
+ *       500:
+ *         description: Erro interno ao deletar o filme
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: "Algo deu errado ao deletar o filme."
+ */
+
 filmsRoute.delete("/:id", filmsController.deleteFilm);
+/**
+ * @openapi
+ * /films/{id}:
+ *   put:
+ *     tags:
+ *       - Filmes
+ *     summary: Atualiza um filme pelo ID
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: ID do filme a ser atualizado
+ *         schema:
+ *           type: string
+ *           example: "abc123"
+ *     requestBody:
+ *       required: true
+ *       description: |
+ *         As chaves do body são opcionais, mas somente **title**, **year** e **description**
+ *         são aceitas no corpo da requisição. Valores nulos retornarão em status 500.
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Film'
+ *           example:
+ *             title: "O Poderoso Chefão"
+ *             year: "1972"
+ *             description: "Um clássico dos filmes de máfia."
+ *     responses:
+ *       200:
+ *         description: Filme atualizado com sucesso
+ *       401:
+ *          description: Não autorizado
+ *          content:
+ *            application/json:
+ *              schema:
+ *                type: object
+ *                properties:
+ *                  message:
+ *                    type: string
+ *                    example: "Não foi possível válidar suas credenciais. Por favor, realize novamente o login"
+ *       500:
+ *         description: Erro interno ao atualizar o filme
+ */
+
 filmsRoute.put("/:id", filmsController.updateFilm);
 
 export default filmsRoute;
